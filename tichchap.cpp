ef8494701ae9cdb0025af12686997779be6694c0
Seito <duongbacdinhthoa@gmail.com>
2023-09-08 00:12:43 +0700

    chua xong

--- tichchap.cpp
+++ tichchap.cpp
@@ -5,56 +5,6 @@
 #include<vector>
 #include<string>
 #include<climits>
-void getin(std::vector<std::vector<int>> &a)
-{
-    int n=a.size();
-    int m=a[0].size();
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<m;j++)    std::cin>>a[i][j];
-    }
-}
-void getin(std::vector<int> &a)
-{
-    int n=a.size();
-    for(int i=0;i<n;i++) std::cin>>a[i];
-}
-int cal(std::vector<std::vector<int>> a,std::vector<int> h,int i,int j)
-{
-    int sum=0;
-    int mark=0;
-    for(int x=i+2;x>=i;x--)
-    {
-        for(int k=j+2;k>=j;k--)
-        {
-            sum+= (a[x][k]*h[mark++]);
-        }
-    }
-    return sum;
-}
-
-void solve(int n,int m)
-{
-    std::vector<std::vector<int>> a(n,std::vector<int>(m,0));getin(a);
-    //std::vector<std::vector<int>> h(3,std::vector<int>(3,0));getin(h);
-    std::vector<int> h(9,0);getin(h);
-    
-
-    int x=n-2,y=m-2;
-    std::vector<std::vector<int>> ans(x,std::vector<int> (y,0));
-    int result=0;
-    for(int i=0;i<x;i++)
-    {
-        for(int j=0;j<y;j++)
-        {
-            result+=cal(a,h,i,j);
-            
-        }
-        
-    }
-    std::cout<<result<<"\n";
-}
-
 int main()
 {
     std::ios_base::sync_with_stdio(false);
@@ -67,9 +17,25 @@ int main()
     {
         int n,m;
         std::cin>>n>>m;
-        solve(n,m);
+        long long a[n][m],h[3][3];
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++) std::cin>> a[i][j];
+        
+        for(int i=0;i<3;i++)
+            for(int j=0;j<3;j++) std::cin>> h[i][j];
+
+        long long sum=0;
+        for(int i=1;i<n-1;i++)
+        {
+            for(int j=1;j<m-1;j++)
+            {
+                long long temp = h[0][0]*a[i+1][j+1] + h[0][1]*a[i+1][j] + h[0][2]*a[i+1][j-1]+
+                                 h[1][0]*a[i][j+1]   + h[1][1]*a[i][j]   + h[1][2]*a[i][j-1]+
+                                 h[2][0]*a[i-1][j+1] + h[2][1]*a[i-1][j] + h[2][2]*a[i-1][j-1];
+                sum+=temp;
+            }
+        }
+        std::cout<<sum<<"\n";
+
     }
-
 }
-
-
