ae2dc85a73692562108c1a83468b84f34e6ee5a5
Seito <duongbacdinhthoa@gmail.com>
2023-09-07 23:50:46 +0700

    sua hang ma tran

--- tinhhangcuamatran2.cpp
+++ tinhhangcuamatran2.cpp
@@ -6,34 +6,14 @@
 #include<string>
 #include<climits>
 
-void Swap(std::vector<std::vector<int>> &a,int row1,int row2)
-{
 
-}
 
-int findRank(std::vector<std::vector<int>> &a)
-{
-    int n= a.size(),m=a[0].size();
-
-    int rank= n;
-
-    for(int row=0;row<rank;row++)
-    {
-        if(a[row][row])
-    }
-}
-
-void solve(int n,int m)
-{
-    std::vector<std::vector<int>> a(n,std::vector<int>(m,0));
-    std::cout<<findRank(a)<<"\n";
-}
 
 int main()
 {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cout.tie(0);
+    // std::ios_base::sync_with_stdio(false);
+    // std::cin.tie(0);
+    // std::cout.tie(0);
 
     int t;
     std::cin>>t;
@@ -41,6 +21,57 @@ int main()
     {
         int n,m;
         std::cin>>n>>m;
-        solve(n,m);
+        std::vector<std::vector<int>> a(n,std::vector<int>(m,0));
+
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++) std::cin>>a[i][j];
+
+        int i=0,j=0;
+        int k,t,rank,save;
+
+        while(i<n && j<m)
+        {
+            if(a[i][j]==0)
+            {
+                for(t=i+1;t<n;t++)
+                {
+                    if(a[t][j]!=0)
+                    {
+                        for(k=i;k<m;k++)
+                        {
+                            std::swap(a[t][k],a[i][k]);
+                        }
+                        break;
+                    }
+                    
+                }
+                if(t==n) j++;
+            }
+            if(a[i][j]!=0)
+            {
+                for(t=i+1;t<n;t++)
+                {
+                    if(a[t][j]!=0)
+                    {
+                        save= a[t][j];
+                        for(k=j;k<m;k++)
+                            a[t][k]= a[t][k] - a[i][k]*save/a[i][j];
+                    }
+                }
+                i++;j++;
+            }
+        }
+        rank=n;
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                if(a[i][j]!=0) break;
+            }
+            if(j==m) rank--;
+        }
+        std::cout<<rank<<"\n";
+        
     }
+
 }
